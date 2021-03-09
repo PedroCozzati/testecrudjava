0e02606c5f08babf1a7c7535616816ecf533cd1b
pedroh97 <80106385+pedroh97@users.noreply.github.com>
2021-03-09 15:54:04 -0300

    Add files via upload

---
+++ Controle.java
@@ -0,0 +1,59 @@
+public class Controle {
+    
+    private int criar;
+    private int fim;
+    private int ver;
+    private int modificar;
+
+    
+    public Controle(int continuar, int fim, int ver, int modificar) {
+        this.setContinuar(continuar);
+        this.setFim(fim);
+        this.setVer(ver);
+        this.setModificar(modificar);
+    }
+
+
+    public int getModificar() {
+        return modificar;
+    }
+
+
+    public void setModificar(int modificar) {
+        this.modificar = modificar;
+    }
+
+
+    public int getVer() {
+        return ver;
+    }
+
+
+    public void setVer(int ver) {
+        this.ver = ver;
+    }
+
+
+    public int getFim() {
+        return fim;
+    }
+
+
+    public void setFim(int fim) {
+        this.fim = fim;
+    }
+
+
+    public int getContinuar() {
+        return continuar;
+    }
+
+
+    public void setContinuar(int continuar) {
+        this.continuar = continuar;
+    }
+    
+   
+  
+
+}
---
+++ Funcionario.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Funcionario extends Pessoa {
+
+    private static Scanner s;
+    private static Scanner a;
+
+    public static void main(String[] args) {
+
+
+        s = new Scanner(System.in);
+        ArrayList<Pessoa> listaPessoa = new ArrayList<Pessoa>();    
+
+        a = new Scanner(System.in);
+
+        fim = a.nextInt();
+        
+        while (fim != 9999){}
+
+            for(int i=0; i < 1; i++){
+                
+                Pessoa pessoa = new Pessoa();
+                System.out.println("\nDigite o nome:");
+                pessoa.nome = s.next();
+
+                System.out.println("\nDigite a idade:");
+                pessoa.idade = s.nextInt();
+
+                System.out.println("\nDigite a matricula:");
+                pessoa.idade = s.nextInt();
+
+                listaPessoa.add(pessoa);
+
+            }
+        }
+
+        System.out.println(listaPessoa);
+    }
+
+}
